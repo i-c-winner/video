7d746bc2792837b53cbd7c2c5fbbf456e9f03a30
Dmitriy Belousov <d-belousov@hotmail.com>
2023-08-27 00:03:48 +0500

    first commit

---
+++ src/app/main.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+import React from "react";
+import {App} from "./module/App";
+
+const container =document.getElementById("root")
+ReactDOM.createRoot(container).render(<App />)
