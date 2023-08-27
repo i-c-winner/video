7567580dc9ef954554f2626e4557af757d4dda83
Dmitriy Belousov <d-belousov@hotmail.com>
2023-08-27 06:57:18 +0500

    feat: Создаст класс Xmpp
    * Создана функция подключения к XMPP серверу

--- src/app/module/App.tsx
+++ src/app/module/App.tsx
@@ -1,3 +1,7 @@
+import { Xmpp } from "../../entities/conference/xmpp";
+const xmpp= new Xmpp('https://xmpp.prosolen.net:5281/http-bind')
+xmpp.register('dsf', 'sdfsdf')
+
 function App () {
   return <p>APP</p>
 }
---
+++ src/entities/conference/xmpp.ts
@@ -0,0 +1,57 @@
+import * as strophe from "strophe.js"
+import { setRegister } from "../../shared/lib/setRegister";
+
+const { Strophe }: any = strophe
+setRegister(strophe)
+console.log(Strophe)
+
+class Xmpp {
+  private connection: any;
+  private static instance: any;
+
+  constructor(url: string) {
+    if (!Xmpp.instance) {
+      Xmpp.instance = this
+
+    }
+    debugger
+    this.connection = new Strophe.Connection(url)
+    return Xmpp.instance
+  }
+
+  register(userNode: string, password: string) {
+    const callback = (status: number) => {
+      //@ts-ignore
+      if (status === Strophe.Status.REGISTER) {
+        // fill out the fields
+        this.connection.register.fields.username = "sddfsdfsdfdf1";
+        this.connection.register.fields.password = "dfsdsdfdsffs1";
+        // calling submit will continue the registration process
+        this.connection.register.submit();
+        //@ts-ignore
+      } else if (status === Strophe.Status.REGISTERED) {
+        console.log("registered!");
+        // calling login will authenticate the registered JID.
+        this.connection.authenticate();
+        //@ts-ignore
+      } else if (status === Strophe.Status.CONFLICT) {
+        console.log("Contact already existed!");
+        //@ts-ignore
+      } else if (status === Strophe.Status.NOTACCEPTABLE) {
+        console.log("Registration form not properly filled out.")
+        //@ts-ignore
+      } else if (status === Strophe.Status.REGIFAIL) {
+        console.log("The Server does not support In-Band Registration")
+      } else if (status === Strophe.Status.CONNECTED) {
+        console.log('connected')
+
+        // do something after successful authentication
+      } else {
+        // Do other stuff
+      }
+    }
+    this.connection.register.connect("prosolen.net", callback)
+  }
+}
+
+export { Xmpp }
