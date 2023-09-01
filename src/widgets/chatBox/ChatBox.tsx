9364173ef323396f2af89c63b3894ec8a9e34bc2
Dmitriy Belousov <d-belousov@hotmail.com>
2023-09-01 07:48:44 +0500

    style(ChatBox): Изменить стиль для текстАреа

--- src/widgets/chatBox/ChatBox.tsx
+++ src/widgets/chatBox/ChatBox.tsx
@@ -1,38 +1,41 @@
 import { Box, Button } from '@mui/material';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { ChatCard } from '../../entities/chatCard/ChatCard';
 import { useRef } from 'react';
 
 function ChatBox(props: { chatBoxVisible: boolean }) {
-  const [text, setText]= useState<string>('kkk')
-  const refText=useRef<HTMLDivElement>(null)
+  const [ text, setText ] = useState<string>('kkk');
+  const refText = useRef<HTMLDivElement>(null);
+
   function changeText(event: any) {
-    setText(event.target.value)
+    setText(event.target.value);
   }
+
   function sendText() {
-    setText('')
+    setText('');
   }
-    return props.chatBoxVisible ? <Box sx={
-      {
-        display: 'flex',
-        flexFlow: 'column',
-        justifyContent: 'space-between',
-        paddingBottom: '10px',
-        width: '250px',
-        backgroundColor: 'background.paper',
-        paddingTop: '85px'
-      }
+
+  return props.chatBoxVisible ? <Box sx={
+    {
+      display: 'flex',
+      flexFlow: 'column',
+      justifyContent: 'space-between',
+      paddingBottom: '10px',
+      width: '250px',
+      backgroundColor: 'background.paper',
+      paddingTop: '85px'
     }
-    >
-      <Box>
-        <ChatCard/>
-        <ChatCard/>
-      </Box>
-      <Box>
-<textarea className="textarea textarea_chat-box" value={text}  onChange={changeText}/>
-        <Button onClick={sendText} variant="contained">Send</Button>
-      </Box>
-    </Box> : null;
+  }
+  >
+    <Box>
+      <ChatCard/>
+      <ChatCard/>
+    </Box>
+    <Box>
+      <textarea className="textarea textarea_chat-box" value={text} onChange={changeText}/>
+      <Button onClick={sendText} variant="contained">Send</Button>
+    </Box>
+  </Box> : null;
 }
 
 export { ChatBox };
