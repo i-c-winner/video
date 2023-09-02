419839582849fccf3e6544fdc83eb066e592e668
Dmitriy Belousov <d-belousov@hotmail.com>
2023-09-02 21:37:52 +0500

    feat: Создаст вкладку видео в модальном окне настроек

---
+++ src/shared/config/constants.ts
@@ -0,0 +1,26 @@
+const constants = {
+  modal: {
+    WIDTH_HEIGHT: '900px',
+    WIDTH_MIDDLE: '700px',
+    WIDTH_LOW: '400px'
+  },
+  videoQuantity: {
+    VIDEO_HEIGHT: {
+      video: 1200,
+      height: 640,
+      frameRate: 60
+    },
+    VIDEO_MIDDLE: {
+      width: 640,
+      height: 480,
+      frameRate: 50,
+    },
+    VIDEO_LOW: {
+      width: 320,
+      height: 240,
+      frameRate: 30
+    }
+  }
+};
+
+export {constants}
--- src/widgets/modal/settingsChildren/SettingsVideo.tsx
+++ src/widgets/modal/settingsChildren/SettingsVideo.tsx
@@ -1,13 +1,52 @@
-import {ISettingsProps} from '../../types';
+import { ISettingsProps } from '../../types';
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material';
+import { constants } from '../../../shared/config/constants';
+import { Conference } from '../../../functions/Conference';
+const conference= new Conference()
+
+const qualityVideo = {
+  HEIGHT: "Высокое",
+  MIDDLE: "Среднее",
+  LOW: "Низкое"
+};
 
 function SettingsVideo(props: ISettingsProps) {
-const {value, index}=props
+  function changeButton(event: any) {
+    switch (event.target.value) {
+      case qualityVideo.HEIGHT:
 
- return value===index&& (
-   <p>video</p>
- )
+        break
+      case qualityVideo.MIDDLE:
 
+        break
+      case qualityVideo.LOW:
 
+        break
+      case "disabledVideo":
+    }
+  }
+
+  const { value, index } = props;
+  return value === index && (
+    <FormControl>
+      <FormLabel id="demo-radio-buttons-group-label">Качество видео</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        /**
+        *DO TO Необходимо определить начальное качество видео в
+        зависимости от настроек
+        */
+        defaultValue={qualityVideo.MIDDLE}
+        name="radio-buttons-group"
+        onChange={changeButton}
+      >
+        <FormControlLabel value={qualityVideo.HEIGHT} control={<Radio/>} label={qualityVideo.HEIGHT}/>
+        <FormControlLabel value={qualityVideo.MIDDLE} control={<Radio/>} label={qualityVideo.MIDDLE}/>
+        <FormControlLabel value={qualityVideo.LOW} control={<Radio/>} label={qualityVideo.LOW}/>
+        <FormControlLabel value="disabledVideo" control={<Radio/>} label="disable video"/>
+      </RadioGroup>
+    </FormControl>
+  );
 }
 
-export {SettingsVideo}
+export { SettingsVideo };
