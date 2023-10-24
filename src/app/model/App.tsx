115dd3199428769462dfe717bbf9c3c85165d26a
Dmitriy Belousov <d-belousov@hotmail.com>
2023-10-24 10:12:37 +0500

    feat: Задиспачит roomName  и displayName

--- src/app/model/App.tsx
+++ src/app/model/App.tsx
@@ -2,17 +2,17 @@ import { xmpp } from '../../features/conference/xmpp';
 import { Room } from '../../features/room/room';
 import {Provider} from 'react-redux';
 import { store } from '../store/store';
+import { Loginin } from '../../page/model/Loginin';
+
 
 console.log('APp');
 const room = new Room(xmpp);
 xmpp.init(room);
-
-
 function App() {
   console.log('APp 01');
 
   return <Provider store={store}>
-    
+    <Loginin />
   </Provider>;
 }
 
--- src/page/model/Loginin.tsx
+++ src/page/model/Loginin.tsx
@@ -1,4 +1,26 @@
+import { useRef } from 'react';
+import {useDispatch} from 'react-redux';
+import {changeUser} from '../../app/store/configSlice';
+
 function Loginin () {
-return <>Loginin</>
+  const refLogin=useRef<HTMLInputElement>(null)
+  const refRoom=useRef<HTMLInputElement>(null)
+  const dispatch=useDispatch()
+  function changeLogin(event: any) {
+    if (refLogin.current!==null) {
+      dispatch(changeUser({key: 'displayName', value: event.target.value}))
+    }
+  }
+  function changeRoom(event: any) {
+if (refRoom!==null) {
+  dispatch(changeUser({key: 'roomName', value: event.target.value}))
+}
+  }
+return (
+  <div>
+    <input ref={refLogin} onChange={changeLogin}/>
+    <input ref={refRoom} onChange={changeRoom}/>
+  </div>
+)
 }
 export {Loginin}
