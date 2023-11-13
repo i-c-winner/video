1a27da56c17e651b7e11d160a5ed652fab4776c6
Dmitriy Belousov <d-belousov@hotmail.com>
2023-11-13 21:23:00 +0500

    feat: Создаст логику отправки сообщений

--- src/entity/model/ChatInputField.tsx
+++ src/entity/model/ChatInputField.tsx
@@ -1,12 +1,32 @@
 import { Box, Button, TextField } from '@mui/material';
 import {styles} from '../../widgets/styles/styles';
+import { useRef, useState } from 'react';
+import {useDispatch} from 'react-redux';
+import {addChat} from '../../app/store/chatsSlice';
 
 function ChatInputField() {
-
+  const [text, setText]= useState<string>('')
+  const dispatch= useDispatch()
+  function sendMessage() {
+    console.log(refInput.current?.value)
+    dispatch(addChat({
+      text: refInput.current?.value||'text ',
+      author: 'I AM ',
+      id: 'dd'
+    }))
+    setText('')
+  }
+  function changeText(event: any) {
+   setText(event.target.value)
+  }
+const refInput=useRef<HTMLTextAreaElement>(null)
   return (
     <Box sx={styles.chatsboxLayer.chatInputField}>
       <Box component='form'>
         <TextField
+          onChange={changeText}
+          value={text}
+          inputRef={refInput}
           id="standard-multiline-flexible"
           label="Multiline"
           multiline
@@ -15,7 +35,7 @@ function ChatInputField() {
         />
       </Box>
 
-      <Button variant="contained">Send</Button>
+      <Button onClick={sendMessage} variant="contained">Send</Button>
     </Box>
   );
 }
