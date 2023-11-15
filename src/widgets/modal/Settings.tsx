7993f2129857948f4ed609c9e52b885c79556373
Dmitriy Belousov <d-belousov@hotmail.com>
2023-11-15 20:54:51 +0500

    feat: Создаст настроики видео (модальное окно)

--- src/widgets/modal/Settings.tsx
+++ src/widgets/modal/Settings.tsx
@@ -3,11 +3,23 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { ButtonGroup, Button } from '@mui/material';
 
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
   value: number;
+}
+
+
+function getvideo() {
+  return <ButtonGroup sx={{
+    textAlign: 'center'
+  }} variant="outlined" aria-label="outlined button group">
+    <Button>One</Button>
+    <Button>Two</Button>
+    <Button>Three</Button>
+  </ButtonGroup>;
 }
 
 function CustomTabPanel(props: TabPanelProps) {
@@ -22,8 +34,16 @@ function CustomTabPanel(props: TabPanelProps) {
       {...other}
     >
       {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+        <Box sx={{
+          p: 3,
+          bgcolor: 'background.paper',
+          width: '50%',
+          margin: '0 auto',
+          height: '25vh',
+          paddingTop: '15px',
+          boxSizing: 'border-box'
+        }}>
+          {children}
         </Box>
       )}
     </div>
@@ -37,24 +57,36 @@ function a11yProps(index: number) {
   };
 }
 
-const Settings=React.forwardRef((props, ref)=> {
-  const [value, setValue] = React.useState(0);
+const Settings = React.forwardRef((props, ref) => {
+  const [ value, setValue ] = React.useState(0);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+    <Box sx={{
+      width: '100%',
+      paddingTop: '20vh'
+    }}>
+      <Box sx={{
+        borderBottom: 1,
+        borderColor: 'divider',
+        margin: '0 auto',
+        bgcolor: 'background.paper',
+        padding: '10px 20px',
+        width: '50%',
+        boxSizing: 'border-box',
+        textAlign: 'center'
+      }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Video" {...a11yProps(0)} />
           <Tab label="Item Two" {...a11yProps(1)} />
           <Tab label="Item Three" {...a11yProps(2)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        Odin
+        {getvideo()}
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         Item Two
@@ -64,6 +96,6 @@ const Settings=React.forwardRef((props, ref)=> {
       </CustomTabPanel>
     </Box>
   );
-})
+});
 
-export {Settings}
+export { Settings };
