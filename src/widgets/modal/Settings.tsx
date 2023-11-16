c04ce0edb925e535b51dd633f37ab6d7a509c6cc
Dmitriy Belousov <d-belousov@hotmail.com>
2023-11-16 08:58:42 +0500

    feat: Создаст закладку настройки аудио

--- src/widgets/modal/Settings.tsx
+++ src/widgets/modal/Settings.tsx
@@ -3,7 +3,14 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { ButtonGroup, Button } from '@mui/material';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+import { BaseSyntheticEvent, useState } from 'react';
+import { IVideiQty, IAudioQty } from '../type';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -11,20 +18,22 @@ interface TabPanelProps {
   value: number;
 }
 
-
-function getvideo() {
-  return <ButtonGroup sx={{
-    textAlign: 'center'
-  }} variant="outlined" aria-label="outlined button group">
-    <Button>One</Button>
-    <Button>Two</Button>
-    <Button>Three</Button>
-  </ButtonGroup>;
+const videoQty: Readonly<IVideiQty> ={
+  disabled: 'disabled',
+  low: 'low',
+  middle: 'middle',
+  height: 'height'
 }
+const audioQty: Readonly<IAudioQty> = {
+  disabled: 'disabled',
+  enabled: 'enabled'
+}
+
+
+
 
 function CustomTabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props;
-
   return (
     <div
       role="tabpanel"
@@ -57,9 +66,56 @@ function a11yProps(index: number) {
   };
 }
 
+function changeQty(event: BaseSyntheticEvent) {
+  console.log(event.target.value)
+}
+function changeAudio(event: BaseSyntheticEvent){
+  console.log(event.target.value)
+}
 const Settings = React.forwardRef((props, ref) => {
   const [ value, setValue ] = React.useState(0);
-
+  const [currentAudio, setCurrentAudio]= useState<keyof typeof audioQty>(audioQty.enabled)
+  const [currentVideoQty, setCurrentVideoQty]= useState<keyof typeof videoQty>(videoQty.middle)
+  function getvideo() {
+      return (
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Качество видео</FormLabel>
+          <RadioGroup
+            sx={{
+              pointerEvents: 'initial'
+            }}
+            onChange={changeQty}
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue={currentVideoQty}
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value={videoQty.disabled} control={<Radio />} label={videoQty.disabled} />
+            <FormControlLabel value={videoQty.low} control={<Radio />} label={videoQty.low}/>
+            <FormControlLabel value={videoQty.middle} control={<Radio />} label={videoQty.middle}/>
+            <FormControlLabel value={videoQty.height} control={<Radio />} label={videoQty.height} />
+          </RadioGroup>
+        </FormControl>
+      );
+  }
+  function getAudio() {
+    return (
+      <FormControl>
+        <FormLabel id="demo-radio-buttons-group-label">Отключение аудио</FormLabel>
+        <RadioGroup
+          sx={{
+            pointerEvents: 'initial'
+          }}
+          onChange={changeAudio}
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue={currentAudio}
+          name="radio-buttons-group"
+        >
+          <FormControlLabel value={audioQty.disabled} control={<Radio />} label={audioQty.enabled}/>
+          <FormControlLabel value={audioQty.enabled} control={<Radio />} label={audioQty.disabled} />
+        </RadioGroup>
+      </FormControl>
+    );
+  }
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
@@ -67,9 +123,11 @@ const Settings = React.forwardRef((props, ref) => {
   return (
     <Box sx={{
       width: '100%',
-      paddingTop: '20vh'
+      paddingTop: '20vh',
+      pointerEvents: 'none'
     }}>
       <Box sx={{
+        pointerEvents: 'initial',
         borderBottom: 1,
         borderColor: 'divider',
         margin: '0 auto',
@@ -81,15 +139,18 @@ const Settings = React.forwardRef((props, ref) => {
       }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
           <Tab label="Video" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Audio" {...a11yProps(1)} />
           <Tab label="Item Three" {...a11yProps(2)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
         {getvideo()}
+        <Typography>
+
+        </Typography>
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
-        Item Two
+        {getAudio()}
       </CustomTabPanel>
       <CustomTabPanel value={value} index={2}>
         Item Three
---
+++ src/widgets/type.ts
@@ -0,0 +1,14 @@
+
+interface IVideiQty {
+    disabled: 'disabled',
+      low: 'low',
+    middle: 'middle',
+    height: 'height'
+}
+
+interface IAudioQty {
+  enabled: 'enabled',
+  disabled: 'disabled'
+}
+
+export type {IVideiQty, IAudioQty}
