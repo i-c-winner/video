fd2f71c550ccf6d543878318601723a32a3f095c
Dmitriy Belousov <d-belousov@hotmail.com>
2023-11-22 22:41:02 +0500

    feat: Добавит чуть логики для отключения камеры кнопки

--- src/entity/model/UI/button/submenuForCamera.tsx
+++ src/entity/model/UI/button/submenuForCamera.tsx
@@ -1,13 +1,25 @@
 import {Paper} from '@mui/material';
 import {ISubmenu} from '../../../types';
+import {useAsync} from 'react-async';
+
+const getDevices= async ()=>{
+  return navigator.mediaDevices.enumerateDevices()
+}
+
+
 
 function SubmenuForCamera (props: ISubmenu) {
+  const {data, error, isPending} = useAsync({promiseFn: getDevices})
 function getStyle() {
   if (props.style) {
     return props.style
   } else {
     return  {}
   }
+}
+if (data) {
+  const cameras=data.filter((element)=>element.kind==='videoinput')
+  return cameras.map((camera)=><p>{camera.label}</p>)
 }
   return (
     <Paper sx={getStyle} >ssdsd</Paper>
