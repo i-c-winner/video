657015ad1ecc8f560ad62297158f93f1dcbd9fd0
Dmitriy Belousov <d-belousov@hotmail.com>
2023-11-27 18:25:52 +0500

    feat: Создаст вкладку devices

--- src/entity/modal/Devices.tsx
+++ src/entity/modal/Devices.tsx
@@ -1,23 +1,65 @@
-import {Box} from '@mui/material';
-import { useEffect, useRef } from 'react';
+import { Autocomplete, Box, TextField } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
 import { glagol } from '../conference/glagol';
 
+const styleInput = {
+  color: 'white',
+  borderRadius: '8px',
+  background: '#181818'
+};
+
 function Devices() {
-  const refVideo=useRef<HTMLVideoElement>(null)
+  const [ videoDevices, setVideoDevices ] = useState<{ label: string }[]>([]);
+  const [ audioDevices, setAudioDevices ] = useState<{ label: string }[]>([]);
+  const [ microphoneDevices, setMicrophoneDevices ] = useState<{ label: string }[]>([]);
+  const refVideo = useRef<HTMLVideoElement>(null);
   useEffect(() => {
-    const stream=new MediaStream()
-    glagol.peerConnection.getSenders().forEach((sender)=>{
-      if (sender.track?.kind==='video' && sender.track?.contentHint!=='detail') {
-        stream.addTrack(sender.track)
+    const stream = new MediaStream();
+    glagol.peerConnection.getSenders().forEach((sender) => {
+      if (sender.track?.kind === 'video' && sender.track?.contentHint !== 'detail') {
+        stream.addTrack(sender.track);
       }
-    })
-    if (refVideo.current) refVideo.current.srcObject=stream
+    });
+    if (refVideo.current) refVideo.current.srcObject = stream;
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      console.log(devices, 'Devices');
+      setVideoDevices(devices.filter((device) => device.kind === 'videoinput'));
+      setAudioDevices(devices.filter((device) => device.kind === 'audiooutput'));
+      setMicrophoneDevices(devices.filter((device) => device.kind === 'audioinput'));
+    });
   }, []);
   return (
-    <Box>
-      <Box ><video autoPlay={true} className='video video_settings' ref={refVideo}/></Box>
-      <Box></Box>
+    <Box sx={{ pointerEvents: 'initial' }} display="flex">
+      <Box>
+        <video autoPlay={true} className="video video_settings" ref={refVideo}/>
+      </Box>
+      <Box
+        sx={{
+          width: '95%',
+          marginLeft: '10px',
+          display: 'flex',
+          flexFlow: 'column',
+          justifyContent: 'space-between'
+        }}>
+        <Autocomplete sx={styleInput} renderInput={(params) => <TextField
+          classes={{
+            root: 'input_devices'
+          }}
+          sx={{ color: 'red' }} {...params} label="video"/>} options={videoDevices}/>
+        <Autocomplete sx={styleInput} renderInput={(params) => <TextField
+          classes={{
+            root: 'input_devices'
+          }}{...params} label="audio"/>}
+                      options={audioDevices}/>
+        <Autocomplete sx={styleInput} renderInput={(params) => <TextField
+          classes={{
+            root: 'input_devices'
+          }}{...params} label="microphone"/>}
+                      options={microphoneDevices}/>
+      </Box>
+
     </Box>
-  )
+  );
 }
-export {Devices}
+
+export { Devices };
--- src/widgets/modal/RemoteStreamsBoxTileMode.tsx
+++ src/widgets/modal/RemoteStreamsBoxTileMode.tsx
@@ -31,4 +31,4 @@ function RemoteStreamsBoxTileMode() {
         }
         }
 
-        export {RemoteStreamsBoxTileMode};;
+        export {RemoteStreamsBoxTileMode};
