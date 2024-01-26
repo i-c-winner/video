9c59ead8d990a6c4285a0cacc90508d2d40474d2
Dmitriy Belousov <d-belousov@hotmail.com>
2024-01-26 09:28:22 +0500

    perf: Добавит stun-servers

--- src/entity/conference/glagol.ts
+++ src/entity/conference/glagol.ts
@@ -14,10 +14,28 @@ const { Strophe } = strophe;
 const glagol: IGlagol = {
   listener: {},
   peerConnection: new RTCPeerConnection({
+    iceCandidatePoolSize: 5,
     iceServers: [
+      { urls: 'stun:stun.l.google.com:19302'},
+      { urls: 'stun:vks.knodl.tech:80'},
+
       {
-        urls: 'stun:stun.l.google.com:19302'
-      }
+        urls: 'turn:vks.knodl.tech:80',
+        username: 'nehy$.pth-3084659',
+        credential: 'l@g&wojmv-po5924rufjmfvoi%np58igvao$ifv'
+      },
+
+      {
+        urls: 'turns:vks.knodl.tech:443',
+        username: 'nehy$.pth-3084659',
+        credential: 'l@g&wojmv-po5924rufjmfvoi%np58igvao$ifv'
+      },
+
+      {
+        urls: 'turns:vks.knodl.tech:443?transport=tcp',
+        username: 'nehy$.pth-3084659',
+        credential: 'l@g&wojmv-po5924rufjmfvoi%np58igvao$ifv'
+      },
     ]
   }),
   params: {
