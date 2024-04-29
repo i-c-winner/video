4ee63cbb3f4b099c690be327d277c5bdd149bab0
Dmitriy Belousov <d-belousov@hotmail.com>
2024-04-29 21:06:22 +0500

    feat: Создаст слушатель для входных страниц

--- src/page/model/CreateDisplayName.tsx
+++ src/page/model/CreateDisplayName.tsx
@@ -1,65 +1,76 @@
-import React, { BaseSyntheticEvent, useState } from 'react';
-import { Box, Button, Input, Typography } from '@mui/material';
-import { styles } from '../styles/styles';
-import { useTranslation } from 'react-i18next';
-import { getInputStyles } from '../../features/styles/getInputStyles';
-import { MicrophoneIcon, VideoCameraIcon, VideoCameraSlashIcon } from '@heroicons/react/24/outline';
-import { MicOff } from '@mui/icons-material';
-import { app } from '../../app/model/constants/app';
-import { useNavigate } from 'react-router-dom';
+import React, { BaseSyntheticEvent, useEffect, useState } from "react";
+import { Box, Button, Input, Typography } from "@mui/material";
+import { styles } from "../styles/styles";
+import { useTranslation } from "react-i18next";
+import { getInputStyles } from "../../features/styles/getInputStyles";
+import { MicrophoneIcon, VideoCameraIcon, VideoCameraSlashIcon } from "@heroicons/react/24/outline";
+import { MicOff } from "@mui/icons-material";
+import { app } from "../../app/model/constants/app";
+import { useNavigate } from "react-router-dom";
 
 function CreateDisplayName() {
-  const navigate = useNavigate()
-  const {t, i18n} = useTranslation();
-  const [cameraIsWorking, setcameraIsWorking] = useState<boolean>(true)
-  const [microphoneIsWorking, setMicrophoneIsWorking] = useState<boolean>(true)
+  const navigate = useNavigate();
+  const {t} = useTranslation();
+  const [cameraIsWorking, setcameraIsWorking] = useState<boolean>(true);
+  const [microphoneIsWorking, setMicrophoneIsWorking] = useState<boolean>(true);
   const actions = {
     videoChange: () => {
-      app.startingParameters.cameraIsWorking = !app.startingParameters.cameraIsWorking
-      setcameraIsWorking(!cameraIsWorking)
+      app.startingParameters.cameraIsWorking = !app.startingParameters.cameraIsWorking;
+      setcameraIsWorking(!cameraIsWorking);
     },
     audioChange: () => {
-        app.startingParameters.microphoneIsWorking=!app.startingParameters.microphoneIsWorking
-        setMicrophoneIsWorking(!microphoneIsWorking)
+      app.startingParameters.microphoneIsWorking = !app.startingParameters.microphoneIsWorking;
+      setMicrophoneIsWorking(!microphoneIsWorking);
     }
   };
+  const buttonText: any = "interface.buttons.createDisplayName";
 
   function action(event: BaseSyntheticEvent) {
-    app.displayName = event.target.value
+    app.displayName = event.target.value;
   }
 
   function goPage() {
-    app.appCreated = true
-    navigate(`/${app.roomName}`)
+    app.appCreated = true;
+    navigate(`/${app.roomName}`);
   }
 
+  useEffect(() => {
+    function handlerKey() {
+      goPage();
+    }
+
+    window.addEventListener("keydown", handlerKey);
+    return () => {
+      window.removeEventListener("keydown", handlerKey);
+    };
+  });
   return <Box sx={styles.wrapper}>
     <Input placeholder="input yourName" onChange={action} sx={getInputStyles()}/>
     <Button onClick={goPage}>
-      <Typography variant="myText">{t('interface.buttons.createDisplayName')}</Typography>
+      <Typography variant="myText">{t(buttonText)}</Typography>
     </Button>
     <Box sx={{
-      display: 'flex', justifyContent: 'center',
-      marginTop: '10px',
+      display: "flex", justifyContent: "center",
+      marginTop: "10px",
     }}>
       <Box onClick={actions.videoChange}
            sx={
              {
-               width: '25px',
-               height: '25px',
-               marginRight: '10px'
+               width: "25px",
+               height: "25px",
+               marginRight: "10px"
              }
-           }>{cameraIsWorking ? <VideoCameraIcon color={'green'}/> :
+           }>{cameraIsWorking ? <VideoCameraIcon color={"green"}/> :
         <VideoCameraSlashIcon color="red"/>}
       </Box>
       <Box onClick={actions.audioChange}
            sx={
-        {
-          width: '25px',
-          height: '25px'
-        }
-      }>{microphoneIsWorking ? <MicrophoneIcon color={'green'}/> :
-        <Box sx={{color: 'red'}}><MicOff/></Box>}
+             {
+               width: "25px",
+               height: "25px"
+             }
+           }>{microphoneIsWorking ? <MicrophoneIcon color={"green"}/> :
+        <Box sx={{color: "red"}}><MicOff/></Box>}
       </Box>
     </Box>
   </Box>;
--- src/page/model/CreateRoomName.tsx
+++ src/page/model/CreateRoomName.tsx
@@ -1,28 +1,42 @@
-import { BaseSyntheticEvent, useEffect, useRef } from 'react';
-import { Box, Input, Typography, Button } from '@mui/material';
-import { styles } from '../styles/styles';
-import { getInputStyles } from '../../features/styles/getInputStyles';
-import {useTranslation} from 'react-i18next';
-import {useNavigate} from 'react-router-dom';
-import {app} from '../../app/model/constants/app';
+import { BaseSyntheticEvent, useEffect, useRef } from "react";
+import { Box, Input, Typography, Button } from "@mui/material";
+import { styles } from "../styles/styles";
+import { getInputStyles } from "../../features/styles/getInputStyles";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import { app } from "../../app/model/constants/app";
 
 
 function CreateRoomName() {
-    const navigate=useNavigate()
-    const {t}=useTranslation()
-    const refInput=useRef<HTMLElement>()
-    function change(event: BaseSyntheticEvent){
-        app.roomName=event.target.value
+  const navigate = useNavigate();
+  const {t} = useTranslation();
+  const refInput = useRef<HTMLElement>();
+
+  function change(event: BaseSyntheticEvent) {
+    app.roomName = event.target.value;
+  }
+
+  const buttonText: any = "interface.buttons.createRoomName";
+
+  function actionClick() {
+    navigate(`/creatername`);
+  }
+
+  useEffect(() => {
+    function handlerKey() {
+      actionClick();
     }
-    function actionClick() {
-        navigate(`/creatername`)
-    }
-    return <Box sx={ styles.wrapper }>
-        <Input placeholder="Input RoomName" onChange={ change } sx={ getInputStyles() } ref={refInput}/>
-        <Button onClick={actionClick}>
-            <Typography variant='myText' >{t('interface.buttons.createRoomName')}</Typography>
-        </Button>
-    </Box>;
+    window.addEventListener("keydown", handlerKey);
+    return () => {
+      window.removeEventListener("keydown", handlerKey);
+    };
+  });
+  return <Box sx={styles.wrapper}>
+    <Input placeholder="Input RoomName" onChange={change} sx={getInputStyles()} ref={refInput}/>
+    <Button onClick={actionClick}>
+      <Typography variant="myText">{t(buttonText)}</Typography>
+    </Button>
+  </Box>;
 }
 
 export { CreateRoomName };
